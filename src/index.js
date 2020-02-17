f239b6948fc9254c3d1b1e36e86e0a7d405deb83
greciagarciav <garciav.grecia@gmail.com>
2020-02-16 21:49:14 -0500

    agregando algunas funciones DOM a index.js y fanciones principales a validator.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import validator from './validator.js';
 
+const btnVerifyCardNum = document.getElementById('btn-verifing');
+const maskifyNumber = document.getElementById('card-number');
+const cleanInputCard = document.getElementById('card-number');
+
+maskifyNumber.addEventListener('keyup', (e) => {
+//  alert('hola');
+});
+
+btnVerifyCardNum.addEventListener('click', () => {
+ 
+  let card =  document.getElementById('card-number').value;
+  let validatorNumber = validator.isValid(card);
+  let showResult = document.getElementById('card-result');
+  let validResult =  `<div class="square card-result-valid"><p>${ card}, es válida.</p></div>`;
+  let invalidResult = `<div class="square card-result-invalid"><p> ${ card}, no es válida.</p></div>`;
+
+    if(card == ''){
+      document.getElementById('tip').innerHTML = `Complete este campo`;
+      document.getElementById('tip').classList.add('tool-tip');
+      document.getElementById('card-number').value = '';
+    } else if (card.length !== 16){
+      document.getElementById('tip').innerHTML = `Ingrese 16 digitos`;
+      document.getElementById('tip').classList.add('tool-tip');
+      document.getElementById('card-number').value = '';
+    } else if (validatorNumber == true) {
+      showResult.innerHTML = validResult;
+      document.getElementById('card-number').value = '';
+    } else {
+      showResult.innerHTML = invalidResult;
+      document.getElementById('card-number').value = '';
+    }
+});
+
+cleanInputCard.addEventListener('keypress', () => {
+  document.getElementById('card-result').innerHTML = ``;
+  document.getElementById('tip').innerHTML = ``;
+  document.getElementById('tip').classList.remove('tool-tip');
+})
+
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,65 @@
 const validator = {
   
+  isValid : (cardNumber) => {
+    
+    let numberReverted = cardNumber.split("").reverse();
+  
+    let result= [];
+    for(let i = 0; i < numberReverted.length ; i++){
+        let j = 0;
+        if(i % 2 !== 0){
+         result.push(numberReverted[i] * 2);
+        }
+         else {
+        result.push(parseInt(numberReverted[i]));
+        }   
+    } 
+    // console.log(result);
+  
+    let newArr = [];
+    for(let i = 0; i < result.length; i++){
+      let arr1;
+      let sum12;
+      if(result[i]>9){
+          arr1 = result[i].toString().split("");
+          sum12 = parseInt(arr1[0]) + parseInt(arr1[1]);
+        newArr.push(sum12); 
+      } else {
+        newArr.push((result[i]));
+      }
+    }
+    // console.log(newArr);
+  
+    let initial = 0;
+    let total = newArr.reduce((sum, current) => sum + current, initial);
+  
+    // console.log(total);
+    
+    if(total % 10 == 0){
+      return true;
+    } else {
+      return false;
+    }
+  
+  },
+
+
+  maskify : (creditCardNumber) => {
+
+    let num = creditCardNumber.split("");
+    let result="";
+  
+    for(var i = 0; i < num.length; i++){
+   
+     if(i < (num.length - 4)){
+       result +="#";
+     } else {
+       result += creditCardNumber[i];
+     }
+    }
+    return result;
+  },
+
 };
 
 export default validator;
